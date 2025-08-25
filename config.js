3b760fb602ebd3d301c97a0afd971d18a8cd153e
Rūta <rutagaiz@gmail.com>
2025-08-25 13:22:49 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    github: {
-        username: "rutagaiz",
-        repo_name: "neetcode_progress",
-        token: "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",
-        committer_name: "Ruta",
-        committer_email: "rutagaiz@example.com"
-    }
-};
